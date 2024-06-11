258ba29e86f8c8642c8aa7b0be6c89555eb68d08
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-11 13:36:35 +0200

    parse function

--- main.cpp
+++ main.cpp
@@ -16,12 +16,45 @@ using namespace std;
 struct Player
 {
     std::string team_name;
-    std::string role;
+    std::string playmode;
     std::string side;
     int unum;
     double x;
     double y;
 };
+
+
+// Parse initial message function (init Side Unum PlayMode)
+Player parseInitialMessage(const std::string &message, Player &player)
+{
+    std::string delimiter = " ";
+    size_t pos = 0;
+    std::string token;
+    int i = 0;
+    while ((pos = message.find(delimiter)) != std::string::npos)
+    {
+        token = message.substr(0, pos);
+        if (i == 1)
+        {
+            player.side = token;
+        }
+        else if (i == 2)
+        {
+            player.unum = std::stoi(token);
+        }
+        else if (i == 3)
+        {
+            player.playmode = token;
+        }
+        message.erase(0, pos + delimiter.length());
+        i++;
+    }
+    cout << "Player: " << player.side << " " << player.unum << " " << player.playmode << endl;
+    return player;
+}
+
+
+
 
 // Move command function
 void sendInitialMoveMessage(const Player &player, MinimalSocket::udp::Udp<true> &udp_socket, MinimalSocket::Address const &recipient)
@@ -95,12 +128,11 @@ int main(int argc, char *argv[])
     MinimalSocket::Address other_sender_udp = received_message->sender;
     MinimalSocket::Address server_udp = MinimalSocket::Address{"127.0.0.1", other_sender_udp.getPort()};
 
-/*
-    Player player{team_name, "striker", "left", 1, 0, 0};
-    //player = Parser::parseInitialMessage(received_message_content, player);
-    //cout << player << endl;
-    sendInitialMoveMessage(player, udp_socket, server_udp);
 
-*/
+    Player player
+    player = Parser::parseInitialMessage(received_message_content, player);
+    cout << player << endl;
+    //sendInitialMoveMessage(player, udp_socket, server_udp);
+
 return 0;
 }
